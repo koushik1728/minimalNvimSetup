56e025c9d0552ef74f3f0aacad0006e485c3c49b
cookieNixOSKDE <koushiknemali1728@gmail.com>
2026-08-02 16:14:49 +0530

    without lazynvim

---
+++ init.lua
@@ -0,0 +1,6 @@
+require("vim._core.ui2").enable({})
+
+require("options")
+require("keymaps")
+vim.pack.add({ "https://github.com/bluz71/vim-moonfly-colors" })
+
---
+++ lua/keymaps.lua
@@ -0,0 +1,35 @@
+vim.g.mapleader = " "
+
+--pasting without losing the copied text
+vim.keymap.set("x", "p", [["_dP]], { desc = "paste over selection without losing yanked text"})
+
+-- deletion without saving to copy register
+vim.keymap.set({"n", "v"}, "<leader>d" ,[["_d]], { desc = "Delete without yanking"})
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search Highlighting"})
+
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection"})
+
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv", { desc = "moves lines up in visual selection"})
+
+vim.keymap.set("v", "<" ,"<gv", {desc = "Unindent and keep selection"})
+vim.keymap.set("v", ">" ,">gv", {desc = "Unindent and keep selection"})
+
+vim.keymap.set("n", "<C-d>" ,"<C-d>zz", {desc = "Keep the cursor centered and scroll half page down"})
+vim.keymap.set("n", "<C-u>" ,"<C-u>zz", {desc = "Unindent and keep selection"})
+
+
+vim.keymap.set("n", "n" ,"nzzzv", {desc = "Next search result is centered"})
+vim.keymap.set("n", "N" ,"Nzzzv", {desc = "Previous search result is centered"})
+
+vim.keymap.set("n", "<leader>s" ,[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc = "Replace word split screen when using :%s/*/*/g"})
+vim.keymap.set("n", "<leader>X" ,"<cmd>!chmod +x %<CR>", {desc = "Makes file executable"})
+
+vim.keymap.set("n", "<leader>re" ,"<cmd>restart<cr>", {desc = "Makes file executable"})
+
+vim.keymap.set("n","<leader>u", function()
+    vim.cmd.packadd("nvim.undotree")
+    require("undotree").open()
+end, { desc= "Toggle Builtin Undotree"})
